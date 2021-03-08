bf5a9a729f83b591f88b64cf0571af2d2a1d34df
rogbest3 <rogbest3@gmail.com>
2021-03-08 11:19:15 +0900

    210308 11:15 commit
    JavaScript 문법
     - md 파일

---
+++ js-study/src/App.js
@@ -0,0 +1,7 @@
+import JSChapter2 from "./JavaScript/chapter2/JSChapter2";
+
+function App() {
+  return <JSChapter2 />;
+}
+
+export default App;
---
+++ js-study/src/JavaScript/chapter2/JSChapter2.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const JSChapter2 = () => {
+  // 삼항 연산자
+  const test01 = () => {
+    return;
+  };
+  return <div></div>;
+};
+
+export default JSChapter2;
